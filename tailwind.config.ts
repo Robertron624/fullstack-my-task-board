df7885c12c1676189d00585f627a6815058f442a
Robertron624 <robert.624@hotmail.com>
2024-07-25 13:40:47 -0500

    chore: configuring starting project
    Adding project assets, fonts and colors and changing the page title and description

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'very-light-gray': '#F8FAFC',
+        'light-yellow': '#F5D565',
+        'light-beige': '#F5E8D5',
+        'orange': '#E9A23B',
+        'light-green': '#A0ECB1',
+        'green': '#32D657',
+        'light-pink': '#F7D4D3',
+        'red': '#DD524C',
+        'light-blue-gray': '#E3E8EF',
+        'medium-gray': '#97A3B6',
+        'blue': '#3662E3',
+        'semi-transparent-black': '#00000033'
+      }
     },
   },
   plugins: [],
